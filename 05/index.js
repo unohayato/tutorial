7d3ce98388c82f156765ad9d5b3e24136f7687b2
unohayato <mamidori1p1p@gmail.com>
2023-02-04 20:34:48 +0900

    feat: :sparkles: add api 05

--- 05/index.js
+++ 05/index.js
@@ -2,8 +2,23 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const interests = [
+  {
+    name: "programing",
+    emoji: "💻",
+    score: 80,
+  },
+
+  {
+    name: "soccer",
+    emoji: "⚽️",
+    score: 30,
+  },
+];
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json(interests);
+  //res.send("Hello World!");
 });
 
 app.listen(port, () => {
